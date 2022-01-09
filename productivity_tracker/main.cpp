2e4fb8619c42685f21deb104f91117e0f05d8c41
Leigh Arino de la Rubia <leigh@ppm.io>
2022-01-09 05:11:46 -0500

    Initial Commit

---
+++ productivity_tracker/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  productivity_tracker
+//
+//  Created by Leigh Arino on 1/9/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
